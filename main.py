75a7ee704329c8557dec6b9b8ec91529e026bcc1
TSOlami <tijani.saheed@yahoo.com>
2023-06-16 21:34:15 +0100

    Attempting to create a fuction to store user data

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from webapp import create_app
+from flask import Flask
 
 if __name__ == '__main__':
     app = create_app()
---
+++ webapp/models.py
@@ -0,0 +1,20 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+
+db = SQLAlchemy()
+
+class Transaction():
+    pass
+
+class User(db.Model, UserMixin):
+    """The User Model"""
+    
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(250), unique=True, nullable=False)
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    # transactions = db.relationship('Transaction')
+
+   
+

f74a09530a7777a047aefde564a56ddd58dc7239
TSOlami <tijani.saheed@yahoo.com>
2023-06-20 21:48:04 +0100

    add more variables to the new transaction fuction

--- webapp/views.py
+++ webapp/views.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template, request
+import datetime
+from flask import Blueprint, render_template, request, jsonify
 from flask_login import login_required, current_user
 from sqlalchemy.orm import sessionmaker
 import requests
@@ -7,17 +8,30 @@ views = Blueprint('views', __name__)
 
 @views.route ('/')
 def landing():
-    return render_template("landing.html")
+    return "This is the landing page"
+    # return render_template("landing.html")
 
 @views.route ('/home')
 def home():
     return render_template("home.html", user=current_user)
 
-@views.route('/transactions', methods=['POST','GET'])
-def transactions():
+@views.route('/transactions', methods=['GET','POST'])
+def new_transactions():
     if request.method == 'POST':
-        amount = request.form.get('amount')
-        coin_name = request.form.get('coin_name')
+        coin_name = request.json["coin_name"]
+        amount = request.json["amount"]
+        trans_type = request.json["trans_type"]
+        time_transacted = datetime.fromtimestamp(request.json["time_transacted"])
+        price_purchased_at = float(request.json["price_purchased_at"])
+        time_created = datetime.fromtimestamp(request.json["time_created"])
+        no_of_coins = float(request.json["no_of_coins"])
+        Base.metadata.create_all(engine)
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        new_trans = Transaction(coin_name=coin_name, amount=amount, trans_type=trans_type, time_transacted=time_transacted, price_purchased_at=price_purchased_at, time_created=time_created, no_of_coins=no_of_coins)
+        session.add(new_trans)
+        return jsonify(request.json)
+
         coin_name = coin_name.lower()
         url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest"
         api_key = "05bf26b5-a99a-4eb7-92f4-e2c8bc263693"

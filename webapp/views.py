3189e7582061a649f09e90c368d3e4ba6ca03ab0
TSOlami <tijani.saheed@yahoo.com>
2023-06-22 09:18:19 +0100

    fixing some indentations

--- webapp/views.py
+++ webapp/views.py
@@ -16,41 +16,10 @@ def home():
     user_id = request.args.get('user_id')
     return render_template("home.html", user_id=user_id)
 
-@views.route('/<user_id>/transactions', methods=['GET'])
-def transactions(user_id):
-    coin_name_list = []
-    amount_list = []
-    symbol_list = []
-    price_purchased_at_list = []
-    no_of_coins_list = []
-    time_transacted_list = []
-    time_updated_list = []
-    Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    for trans in session.query(Transaction).all():
-        if trans.user_id == user_id:
-           coin_name_list.append(trans.coin_name)
-           amount_list.append(trans.amount)
-           symbol_list.append(trans.symbol)
-           price_purchased_at_list.append(trans.price_purchased_at)
-           url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest"
-           api_key = "05bf26b5-a99a-4eb7-92f4-e2c8bc263693"
-           headers = {'Accepts' : 'application/json', 'X-CMC_PRO_API_KEY' : api_key}
-           params = { 'start' : '1', 'limit' : '5000', 'convert' : 'USD'}
-           data = requests.get(url, params=params, headers=headers).json()
-           for i in range(0, 5000):
-               if data['data'][i]['name'].lower() == trans.coin_name.lower():
-                   no_of_coins = float(trans.amount) / data['data'][i]['quote']['USD']['price']
-                   no_of_coins_list.append(no_of_coins)
-                   time_transacted_list.append(trans.time_transacted)
-                   time_updated_list.append(trans.time_updated)
-    portfolio_worth = sum(amount_list)
-    session.query(User).get(user_id).portfolio_worth = portfolio_worth
-    return render_template('transactions.html', user_id=user_id, coin_name_list=coin_name_list, amount_list=amount_list, symbol_list=symbol_list, price_purchased_at_list=price_purchased_at_list, no_of_coins_list=no_of_coins_list, time_transacted_list=time_transacted_list, time_updated_list=time_updated_list, portfolio_worth=portfolio_worth)
 
 @views.route('/<user_id>/transactions/add_transaction', methods=['POST'])
 def new_transactions(user_id):
+    """Endpoint add a new transaction"""
     coin_name = request.form.get('coin_name')
     coin_name = coin_name.lower()
     amount = request.form.get('amount')
@@ -87,8 +56,45 @@ def new_transactions(user_id):
             return redirect(url_for("views.transactions", user_id=user_id))
     return "Unable to add Transaction"
 
+
+@views.route('/<user_id>/transactions', methods=['GET'])
+def transactions(user_id):
+    """Endpoint to fetch data from the database"""
+    coin_name_list = []
+    amount_list = []
+    symbol_list = []
+    price_purchased_at_list = []
+    no_of_coins_list = []
+    time_transacted_list = []
+    time_updated_list = []
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for trans in session.query(Transaction).all():
+        if trans.user_id == user_id:
+           coin_name_list.append(trans.coin_name)
+           amount_list.append(trans.amount)
+           symbol_list.append(trans.symbol)
+           price_purchased_at_list.append(trans.price_purchased_at)
+           url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest"
+           api_key = "05bf26b5-a99a-4eb7-92f4-e2c8bc263693"
+           headers = {'Accepts' : 'application/json', 'X-CMC_PRO_API_KEY' : api_key}
+           params = { 'start' : '1', 'limit' : '5000', 'convert' : 'USD'}
+           data = requests.get(url, params=params, headers=headers).json()
+           for i in range(0, 5000):
+               if data['data'][i]['name'].lower() == trans.coin_name.lower():
+                   no_of_coins = float(trans.amount) / data['data'][i]['quote']['USD']['price']
+                   no_of_coins_list.append(no_of_coins)
+                   time_transacted_list.append(trans.time_transacted)
+                   time_updated_list.append(trans.time_updated)
+    portfolio_worth = sum(amount_list)
+    session.query(User).get(user_id).portfolio_worth = portfolio_worth
+    return render_template('transactions.html', user_id=user_id, coin_name_list=coin_name_list, amount_list=amount_list, symbol_list=symbol_list, price_purchased_at_list=price_purchased_at_list, no_of_coins_list=no_of_coins_list, time_transacted_list=time_transacted_list, time_updated_list=time_updated_list, portfolio_worth=portfolio_worth)
+
+
 @views.route('/<user_id>/transactions/update_transaction', methods=['POST'])
 def update_transactions(user_id):
+    """Endpoint to update a transaction"""
     coin_name = request.form.get('coin_name')
     coin_name = coin_name.lower()
     amount = request.form.get('amount')

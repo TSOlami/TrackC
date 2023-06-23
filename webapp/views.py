daaa2bd976289268b8c8ed0100d1d0e660d248b1
TSOlami <tijani.saheed@yahoo.com>
2023-06-23 16:48:36 +0100

    Fixed the transactions table

--- webapp/views.py
+++ webapp/views.py
@@ -1,13 +1,13 @@
 import datetime
 import json
-from flask import Blueprint, render_template, request, jsonify, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for
 from flask_login import login_required, current_user
 from sqlalchemy.orm import sessionmaker
 import requests
 from requests.exceptions import RequestException, ConnectionError, Timeout, TooManyRedirects
 from .models import *
 from datetime import datetime
-#from newsapi import NewsApiClient
+from newsapi import NewsApiClient
 
 views = Blueprint('views', __name__)
 
@@ -22,6 +22,7 @@ def landing():
 
 
 @views.route ('/home/')
+@login_required
 def home():
     # Endpoint to get top 10 crytpo-currencies from CMC
     url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
@@ -31,7 +32,7 @@ def home():
     headers = {'Accepts': 'application/json', 
                'X-CMC_PRO_API_KEY': 
                '05bf26b5-a99a-4eb7-92f4-e2c8bc263693'}
-    # Create a new transaction
+    # Create a db session
     session = requests.Session()
     session.headers.update(headers)
 
@@ -69,10 +70,6 @@ def news():
     return render_template("news.html", context=mylist)
 
 
-@views.route ('/hottests')
-def get_top_10():
-    pass
-
 @views.route('/<user_id>/transactions', methods=['GET'])
 def transactions(user_id):
     """Endpoint to fetch data from the database"""
@@ -83,36 +80,69 @@ def transactions(user_id):
     no_of_coins_list = []
     time_transacted_list = []
     time_updated_list = []
-    new_amount_list = []
+
     # Create a new transaction
-    for trans in session.query(Transaction).all():
-        if trans.user_id == user_id:
-           coin_name_list.append(trans.coin_name)
-           amount_list.append(trans.amount)
-           symbol_list.append(trans.symbol)
-           price_purchased_at_list.append(trans.price_purchased_at)
-           url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest"
-           api_key = "05bf26b5-a99a-4eb7-92f4-e2c8bc263693"
-           headers = {'Accepts' : 'application/json', 'X-CMC_PRO_API_KEY' : api_key}
-           params = { 'start' : '1', 'limit' : '5000', 'convert' : 'USD'}
-           data = requests.get(url, params=params, headers=headers).json()
-           for i in range(0, 5000):
-               if data['data'][i]['name'].lower() == trans.coin_name.lower():
-                   no_of_coins = float(trans.amount) / data['data'][i]['quote']['USD']['price']
-                   no_of_coins_list.append(no_of_coins)
-                   new_amount = float(trans.no_of_coins) * float(data['data'][i]['quote']['USD']['price'])
-                   new_amount_list.append(new_amount)
-                   time_transacted_list.append(trans.time_transacted)
-                   time_updated_list.append(trans.time_updated)
-    portfolio_worth = sum(new_amount_list)
-    session.query(User).get(user_id).portfolio_worth = portfolio_worth
-    trans_list = [coin_name_list, symbol_list, price_purchased_at_list, amount_list, no_of_coins_list, time_transacted_list, time_updated_list]
-    return render_template('transactions.html',
-                           user_id=user_id,
-                           trans_list=trans_list,
-                           length=len(coin_name_list),
-                           portfolio_worth=portfolio_worth
-                           )
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    try:
+        # Fetch data from the database
+        for trans in session.query(Transaction).all():
+            if trans.user_id == user_id:
+                coin_name_list.append(trans.coin_name)
+                amount_list.append(trans.amount)
+                symbol_list.append(trans.symbol)
+                price_purchased_at_list.append(trans.price_purchased_at)
+
+                # Make API request to get coin data
+                url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest"
+                api_key = "05bf26b5-a99a-4eb7-92f4-e2c8bc263693"
+                headers = {'Accepts': 'application/json', 'X-CMC_PRO_API_KEY': api_key}
+                params = {'start': '1', 'limit': '5000', 'convert': 'USD'}
+                data = requests.get(url, params=params, headers=headers).json()
+
+                # Process coin data
+                for i in range(0, 5000):
+                    if data['data'][i]['name'].lower() == trans.coin_name.lower():
+                        no_of_coins = float(trans.amount) / data['data'][i]['quote']['USD']['price']
+                        no_of_coins_list.append(no_of_coins)
+                        time_transacted_list.append(trans.time_transacted)
+                        time_updated_list.append(trans.time_updated)
+
+        # Calculate portfolio worth
+        portfolio_worth = sum(amount_list)
+
+        # Close the session
+        session.close()
+
+        # Assign the length of coin_name_list
+        length = len(coin_name_list)
+
+        # Create a list of dictionaries containing transaction data
+        trans_list = []
+        for i in range(length):
+            trans_dict = {
+                'coin_name': coin_name_list[i],
+                'amount': amount_list[i],
+                'symbol': symbol_list[i],
+                'price_purchased_at': price_purchased_at_list[i],
+                'no_of_coins': no_of_coins_list[i],
+                'time_transacted': time_transacted_list[i],
+                'time_updated': time_updated_list[i]
+            }
+            trans_list.append(trans_dict)
+
+        # Render the template with the fetched data
+        return render_template('transactions.html',
+                               user_id=user_id,
+                               trans_list=trans_list,
+                               portfolio_worth=portfolio_worth,
+                               length=length)  # Pass the length variable to the template
+
+    except (RequestException, ConnectionError, Timeout, TooManyRedirects) as e:
+        # Handle the specific exception and return an appropriate response
+        return f"An error occurred: {str(e)}", 500
 
 
 @views.route('/<user_id>/transactions/add_transaction', methods=['POST'])
@@ -195,6 +225,7 @@ def remove_transaction(user_id):
                     portfolio_worth = float(session.query(User).get(user_id).portfolio_worth) - new_amount
                     session.query(User).get(user_id).portfolio_worth = portfolio_worth
                     session.commit()
+                    session.close()
                     return redirect(url_for("views.transactions", user_id=user_id))
     return "Coin is not pressent in your portfolio"
 

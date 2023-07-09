8744f0012204f5ea24266d9094f2bd24ea156aac
slimanyy <aabdulqudduss@gmail.com>
2023-07-09 02:41:29 +0100

    contact mail, forgot password,token generation, templates

--- webapp/config.py
+++ webapp/config.py
@@ -4,11 +4,20 @@ import os
 load_dotenv()
 
 class ApplicationConfig:
-    
+    #secret key
     SECRET_KEY = os.environ["SECRET_KEY"]
+    #password reset token expiration
+    RESET_TOKEN_EXPIRATION = os.environ["RESET_TOKEN_EXPIRATION"]  # Reset token expiration time in seconds
+    # Gmail configurations
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 465  # TLS port
+    MAIL_USE_TLS = False  # Enable TLS
+    MAIL_USE_SSL = True  # Enable SSL
+    MAIL_USERNAME = os.environ["MAIL_USERNAME"]
+    MAIL_PASSWORD = os.environ["MAIL_PASSWORD"]
     
     # Configure the Flask app to use the PostgreSQL database
     SQLALCHEMY_DATABASE_URI = 'postgresql://juuthoos:GaAe2ZbZU77KorqGd7NgIJKfm5Ya9EpH@dumbo.db.elephantsql.com/juuthoos'
-    SQLALCHEMY_ECHO = True
+    SQLALCHEMY_ECHO = True 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
--- webapp/views.py
+++ webapp/views.py
@@ -1,20 +1,65 @@
-import datetime
+from datetime import datetime, timedelta
+from webapp import mail
 import json
-from flask import Blueprint, flash, render_template, request, redirect, url_for
-from flask_login import login_required, current_user
+import os
 import requests
+from flask import Blueprint, flash, render_template, request, redirect, url_for
+from flask_mail import Message
+from flask_login import login_required, current_user
 from requests.exceptions import RequestException, ConnectionError, Timeout, TooManyRedirects
+from werkzeug.security import generate_password_hash, check_password_hash
 from .models import *
-from datetime import datetime
+from .config import *
 
 views = Blueprint('views', __name__)
 
 LIVE_PRICE_URL = "https://api.coingecko.com/api/v3/simple/price"
 
+# Send the password reset email
 
-@views.route ('/')
+
+@staticmethod
+def send_reset_email(user):
+    token = user.generate_reset_token()
+    reset_url = url_for('views.reset_password', token=token, _external=True)
+    message = f"Click the following link to reset your password: {reset_url}"
+    msg = Message('Password Reset',
+                  sender=os.environ['MAIL_USERNAME'], recipients=[user.email])
+    msg.body = message
+    mail.send(msg)
+
+
+# @views.route('/', methods=['GET', 'POST'])
+# def landing():
+#     return render_template("landing.html")
+
+
+@views.route('/', methods=['GET', 'POST'])
 def landing():
-    return render_template("landing.html")
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        message = request.form['message']
+        # Check if checked checkbox is selected
+        send_copy = 'send_copy' in request.form
+
+        recipients = [os.environ['MAIL_USERNAME']]
+
+        if send_copy:
+            recipients.append(email)
+
+        msg = Message('New Message from Trackc Contact form',
+                      sender=os.environ['MAIL_USERNAME'], recipients=recipients)
+        msg.body = f"Name: {name}\nEmail: {email}\nMessage: {message}"
+
+        try:
+            mail.send(msg)
+            flash('Message sent successfully!', 'success')
+        except Exception as e:
+            flash(
+                'An error occurred while sending the message. Please try again.', 'error')
+            print(str(e))
+    return render_template('landing.html')
 
 
 @views.route('/about')
@@ -35,15 +80,18 @@ def home(user_id):
         'per_page': 10,
         'page': 1
     }
-    
+
     response = requests.get(url, params=parameters)
     data = response.json()
     results = data
 
     for result in results:
-        result['current_price'] = '$ ' + "{:,.2f}".format(result['current_price'])
-        result['total_volume'] = '$ ' + "{:,.2f}".format(result['total_volume'])
-        result['price_change_percentage_24h'] = "{:,.2f}".format(result['price_change_percentage_24h']) + '%'
+        result['current_price'] = '$ ' + \
+            "{:,.2f}".format(result['current_price'])
+        result['total_volume'] = '$ ' + \
+            "{:,.2f}".format(result['total_volume'])
+        result['price_change_percentage_24h'] = "{:,.2f}".format(
+            result['price_change_percentage_24h']) + '%'
 
     coin_name_list = []
     amount_spent_list = []
@@ -82,7 +130,8 @@ def home(user_id):
             if coin_name in data:
                 current_price = data[coin_name]['usd']
                 current_value = current_price * float(trans.no_of_coins)
-                equity = (current_value - (float(trans.no_of_coins) * float(trans.price_purchased_at))) / 100
+                equity = (current_value - (float(trans.no_of_coins)
+                          * float(trans.price_purchased_at))) / 100
                 current_values[coin_name] = current_value
                 equities[coin_name] = equity
 
@@ -109,7 +158,7 @@ def home(user_id):
                            portfolio_equity=portfolio_equity)
 
 
-@views.route ('/<user_id>/news', methods=['GET'])
+@views.route('/<user_id>/news', methods=['GET'])
 @login_required
 def news(user_id):
     # Endpoint to make NewsApi calls
@@ -117,11 +166,13 @@ def news(user_id):
     formatted_data = get_formatted_news_data(news_url)
     return render_template('news.html', news=formatted_data, user_id=user_id)
 
+
 def get_formatted_news_data(news_url):
     response = requests.get(news_url)
     data = response.json()
     formatted_data = format_data(data)
     return formatted_data
+
 
 def format_data(data):
     formatted_data = []
@@ -153,11 +204,10 @@ def transactions(user_id):
     # Fetch data from the database
     user = User.query.get(user_id)
     transactions = Transaction.query.filter_by(user_id=user_id).all()
-    
+
     coin_prices = {}
     for trans in transactions:
         coin_prices[trans.coin_name.lower()] = trans.price_purchased_at
-
 
     # Make API request to get live price for the coin
     url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest"
@@ -176,14 +226,14 @@ def transactions(user_id):
             amount_spent_list.append(trans.amount_spent)
             symbol_list.append(trans.symbol)
             price_purchased_at_list.append(trans.price_purchased_at)
-            no_of_coins = trans.no_of_coins 
+            no_of_coins = trans.no_of_coins
 
-            
             for i in range(0, 5000):
-                if data['data'][i]['name'].lower() == trans.coin_name.lower(): 
+                if data['data'][i]['name'].lower() == trans.coin_name.lower():
                     current_price = data['data'][i]['quote']['USD']['price']
                     current_value = current_price * float(trans.no_of_coins)
-                    equity = (current_value - (float(trans.no_of_coins) * float(trans.price_purchased_at))) / 100
+                    equity = (current_value - (float(trans.no_of_coins)
+                              * float(trans.price_purchased_at))) / 100
                     current_values[coin_name] = current_value
                     equities[coin_name] = equity
                     break
@@ -191,11 +241,10 @@ def transactions(user_id):
             time_transacted_list.append(trans.time_transacted)
             time_updated_list.append(trans.time_updated)
 
-
         # Calculate portfolio worth
         portfolio_worth = sum(current_values.values())
         portfolio_equity = sum(equities.values())
-        
+
         # Sort transactions by time_updated in descending order (most recent on top)
         sorted_transactions = sorted(
             zip(coin_name_list, amount_spent_list, symbol_list, price_purchased_at_list, no_of_coins_list,
@@ -205,12 +254,12 @@ def transactions(user_id):
 
         # Create a list of dictionaries containing transaction data
         trans_list = []
-        
+
         for (
             coin_name, amount_spent, symbol, price_purchased_at, no_of_coins, time_transacted,
             time_updated, current_value, equity
         ) in sorted_transactions:
-            current_value = current_values[coin_name.lower()]  
+            current_value = current_values[coin_name.lower()]
             equity = equities[coin_name.lower()]
             trans_dict = {
                 'coin_name': coin_name,
@@ -235,12 +284,11 @@ def transactions(user_id):
             length=len(trans_list),
             portfolio_worth=portfolio_worth,
             portfolio_equity=portfolio_equity)
-    
+
     except Exception as e:
         # Handle the specific exception and flash an appropriate response
         flash('An error occurred!', category='error')
         return redirect(url_for('views.transactions', user_id=user_id))
-        
 
 
 @views.route('/<user_id>/transactions/add_transaction', methods=['POST'])
@@ -250,7 +298,7 @@ def new_transactions(user_id):
         # Retrieve form data from the frontend
         coin_name = request.form.get('coin_name')
         coin_name = coin_name.lower()
-        no_of_coins = request.form.get('no_of_coins')        
+        no_of_coins = request.form.get('no_of_coins')
         price_purchased_at = request.form.get('price_purchased_at')
         amount_spent = float(price_purchased_at) * float(no_of_coins)
 
@@ -259,18 +307,18 @@ def new_transactions(user_id):
             flash("Please provide all required data.", category="error")
             return redirect(url_for("views.transactions", user_id=user_id))
 
-        # Setup CoinMarketCap API 
+        # Setup CoinMarketCap API
         url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest"
         api_key = "032b9c4e-d442-4fdf-8359-ca6736c4216c"
         headers = {'Accepts': 'application/json', 'X-CMC_PRO_API_KEY': api_key}
         params = {'start': '1', 'limit': '5000', 'convert': 'USD'}
         data = requests.get(url, params=params, headers=headers).json()
-        print (data)
+        print(data)
 
         # Find the cryptocurrency matching the provided name
         for i in range(0, 5000):
             if data['data'][i]['name'].lower() == coin_name:
-                print (data)
+                print(data)
                 current_price = data['data'][i]['quote']['USD']['price']
                 symbol = data['data'][i]['symbol']
 
@@ -278,21 +326,26 @@ def new_transactions(user_id):
                 session = db.session()
 
                 # Check if the transaction already exists for the user and coin
-                existing_trans = Transaction.query.filter_by(user_id=user_id, coin_name=coin_name.capitalize()).first()
+                existing_trans = Transaction.query.filter_by(
+                    user_id=user_id, coin_name=coin_name.capitalize()).first()
                 if existing_trans:
                     # Update the existing transaction
-                    existing_trans.amount_spent = float(existing_trans.amount_spent) + float(amount_spent)
-                    existing_trans.no_of_coins = float(existing_trans.no_of_coins) + float(no_of_coins)
+                    existing_trans.amount_spent = float(
+                        existing_trans.amount_spent) + float(amount_spent)
+                    existing_trans.no_of_coins = float(
+                        existing_trans.no_of_coins) + float(no_of_coins)
                     existing_trans.time_updated = datetime.now()
                     user = User.query.get(user_id)
-                    portfolio_worth = float(user.portfolio_worth) + (float(current_price) * float(no_of_coins))
+                    portfolio_worth = float(
+                        user.portfolio_worth) + (float(current_price) * float(no_of_coins))
                     session.commit()
                     session.close()
                     flash("Transaction added successfully.", category="success")
                 else:
                     # Create a new transaction
                     user = User.query.get(user_id)
-                    portfolio_worth = float(user.portfolio_worth) + (float(current_price) * float(no_of_coins))
+                    portfolio_worth = float(
+                        user.portfolio_worth) + (float(current_price) * float(no_of_coins))
                     new_trans = Transaction(
                         user_id=user_id,
                         amount_spent=amount_spent,
@@ -316,7 +369,7 @@ def new_transactions(user_id):
         # Handle the specific exception and flash an appropriate response
         flash("An error occurred while adding the transaction.", category="error")
         return redirect(url_for("views.transactions", user_id=user_id))
-    
+
 
 @views.route('/<user_id>/transactions/remove_transaction', methods=['POST'])
 @login_required
@@ -330,18 +383,20 @@ def remove_transaction(user_id):
     if not coin_name or not no_of_coins or not price_sold:
         flash("Please provide all required data.", category="error")
         return redirect(url_for("views.transactions", user_id=user_id))
-    
+
     try:
         transactions = Transaction.query.filter_by(user_id=user_id).all()
 
         for trans in transactions:
             if trans.coin_name.lower() == coin_name:
                 new_no_of_coins = float(trans.no_of_coins) - float(no_of_coins)
-                new_amount_spent = float(trans.amount_spent) - float(price_sold)
+                new_amount_spent = float(
+                    trans.amount_spent) - float(price_sold)
                 trans.amount_spent = new_amount_spent
                 trans.no_of_coins = new_no_of_coins
                 trans.time_updated = datetime.now()
-                portfolio_worth = float(User.query.get(user_id).portfolio_worth) - float(price_sold)
+                portfolio_worth = float(User.query.get(
+                    user_id).portfolio_worth) - float(price_sold)
                 User.query.get(user_id).portfolio_worth = portfolio_worth
                 db.session.commit()
                 db.session.close()
@@ -356,3 +411,50 @@ def remove_transaction(user_id):
         # Handle the specific exception and flash an appropriate response
         flash("An error occurred!", category="error")
         return redirect(url_for("views.transactions", user_id=user_id))
+
+
+# route for password reset request
+@views.route('/reset_password_request', methods=['GET', 'POST'])
+def reset_password_request():
+    if request.method == 'POST':
+        email = request.form['email']
+        # Check if the user exists and retrieve their details
+        user = User.query.filter_by(email=email).first()
+
+        if user:
+            # Send the reset email
+            send_reset_email(user)
+
+        flash('An email with instructions to reset your password has been sent.', 'success')
+        return redirect(url_for('auth.login'))
+    return render_template('reset_password_request.html')
+
+# Route for resetting the password
+
+
+@views.route('/reset_password/<token>', methods=['GET', 'POST'])
+def reset_password(token):
+    # Check if the token is valid
+    if User.verify_reset_token(token) is None:
+        flash('That is an invalid or expired token', 'warning')
+        return redirect(url_for('auth.reset_password_request'))
+    else:
+        if request.method == 'POST':
+            password = request.form.get('password')
+            verify_password = request.form.get('verify_password')
+
+            if not password or not verify_password or password != verify_password:
+                flash('Passwords don\'t match.', category='error')
+            elif len(password) < 7:
+                flash('Password must be at least 7 characters.', category='error')
+
+            # Update the user's password
+            user = User.verify_reset_token(token)
+            hashed_new_password = generate_password_hash(
+                password, method='sha256')
+            user.password = hashed_new_password
+            db.session.commit()
+            flash(
+                'Your password has been successfully reset. Please login with your new password.', 'success')
+            return redirect(url_for('auth.login'))
+        return render_template('reset_password.html', token=token)
